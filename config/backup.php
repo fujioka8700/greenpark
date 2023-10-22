e0ff46fe2b259dd4bd76f0a66c2eb3f5fbd26960
fujioka <fujioka8700@gmail.com>
2023-10-22 11:31:03 +0900

    [fix] backup バックアップ完了後の、メール通知先を変更した。

--- config/backup.php
+++ config/backup.php
@@ -2,54 +2,54 @@
 
 return [
 
-    'backup' => [
+  'backup' => [
 
-        /*
+    /*
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+    'name' => env('APP_NAME', 'laravel-backup'),
 
-        'source' => [
+    'source' => [
 
-            'files' => [
+      'files' => [
 
-                /*
+        /*
                  * The list of directories and files that will be included in the backup.
                  */
-                'include' => [
-                    base_path(),
-                ],
+        'include' => [
+          base_path(),
+        ],
 
-                /*
+        /*
                  * These directories and files will be excluded from the backup.
                  *
                  * Directories used by the backup process will automatically be excluded.
                  */
-                'exclude' => [
-                    base_path('vendor'),
-                    base_path('node_modules'),
-                ],
+        'exclude' => [
+          base_path('vendor'),
+          base_path('node_modules'),
+        ],
 
-                /*
+        /*
                  * Determines if symlinks should be followed.
                  */
-                'follow_links' => false,
+        'follow_links' => false,
 
-                /*
+        /*
                  * Determines if it should avoid unreadable folders.
                  */
-                'ignore_unreadable_directories' => false,
+        'ignore_unreadable_directories' => false,
 
-                /*
+        /*
                  * This path is used to make directories in resulting zip-file relative
                  * Set to `null` to include complete absolute path
                  * Example: base_path()
                  */
-                'relative_path' => null,
-            ],
+        'relative_path' => null,
+      ],
 
-            /*
+      /*
              * The names of the connections to the databases that should be backed up
              * MySQL, PostgreSQL, SQLite and Mongo databases are supported.
              *
@@ -79,12 +79,12 @@ return [
              *
              * For a complete list of available customization options, see https://github.com/spatie/db-dumper
              */
-            'databases' => [
-                'mysql',
-            ],
-        ],
+      'databases' => [
+        'mysql',
+      ],
+    ],
 
-        /*
+    /*
          * The database dump can be compressed to decrease disk space usage.
          *
          * Out of the box Laravel-backup supplies
@@ -95,130 +95,130 @@ return [
          *
          * If you do not want any compressor at all, set it to null.
          */
-        'database_dump_compressor' => null,
+    'database_dump_compressor' => null,
 
-        /*
+    /*
          * The file extension used for the database dump files.
          *
          * If not specified, the file extension will be .archive for MongoDB and .sql for all other databases
          * The file extension should be specified without a leading .
          */
-        'database_dump_file_extension' => '',
+    'database_dump_file_extension' => '',
 
-        'destination' => [
+    'destination' => [
 
-            /*
+      /*
              * The filename prefix used for the backup zip file.
              */
-            'filename_prefix' => '',
+      'filename_prefix' => '',
 
-            /*
+      /*
              * The disk names on which the backups will be stored.
              */
-            'disks' => [
-                'local',
-            ],
-        ],
+      'disks' => [
+        'local',
+      ],
+    ],
 
-        /*
+    /*
          * The directory where the temporary files will be stored.
          */
-        'temporary_directory' => storage_path('app/backup-temp'),
+    'temporary_directory' => storage_path('app/backup-temp'),
 
-        /*
+    /*
          * The password to be used for archive encryption.
          * Set to `null` to disable encryption.
          */
-        'password' => env('BACKUP_ARCHIVE_PASSWORD'),
+    'password' => env('BACKUP_ARCHIVE_PASSWORD'),
 
-        /*
+    /*
          * The encryption algorithm to be used for archive encryption.
          * You can set it to `null` or `false` to disable encryption.
          *
          * When set to 'default', we'll use ZipArchive::EM_AES_256 if it is
          * available on your system.
          */
-        'encryption' => 'default',
-    ],
+    'encryption' => 'default',
+  ],
 
-    /*
+  /*
      * You can get notified when specific events occur. Out of the box you can use 'mail' and 'slack'.
      * For Slack you need to install laravel/slack-notification-channel.
      *
      * You can also use your own notification classes, just make sure the class is named after one of
      * the `Spatie\Backup\Notifications\Notifications` classes.
      */
+  'notifications' => [
+
     'notifications' => [
-
-        'notifications' => [
-            \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => ['mail'],
-        ],
-
-        /*
-         * Here you can specify the notifiable to which the notifications should be sent. The default
-         * notifiable will use the variables specified in this config file.
-         */
-        'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
-
-        'mail' => [
-            'to' => 'your@example.com',
-
-            'from' => [
-                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-                'name' => env('MAIL_FROM_NAME', 'Example'),
-            ],
-        ],
-
-        'slack' => [
-            'webhook_url' => '',
-
-            /*
-             * If this is set to null the default channel of the webhook will be used.
-             */
-            'channel' => null,
-
-            'username' => null,
-
-            'icon' => null,
-
-        ],
-
-        'discord' => [
-            'webhook_url' => '',
-
-            /*
-             * If this is an empty string, the name field on the webhook will be used.
-             */
-            'username' => '',
-
-            /*
-             * If this is an empty string, the avatar on the webhook will be used.
-             */
-            'avatar_url' => '',
-        ],
+      \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
+      \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
+      \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
+      \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => ['mail'],
+      \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => ['mail'],
+      \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => ['mail'],
     ],
 
     /*
+         * Here you can specify the notifiable to which the notifications should be sent. The default
+         * notifiable will use the variables specified in this config file.
+         */
+    'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
+
+    'mail' => [
+      'to' => env('MAIL_ADMIN'),
+
+      'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
+      ],
+    ],
+
+    'slack' => [
+      'webhook_url' => '',
+
+      /*
+             * If this is set to null the default channel of the webhook will be used.
+             */
+      'channel' => null,
+
+      'username' => null,
+
+      'icon' => null,
+
+    ],
+
+    'discord' => [
+      'webhook_url' => '',
+
+      /*
+             * If this is an empty string, the name field on the webhook will be used.
+             */
+      'username' => '',
+
+      /*
+             * If this is an empty string, the avatar on the webhook will be used.
+             */
+      'avatar_url' => '',
+    ],
+  ],
+
+  /*
      * Here you can specify which backups should be monitored.
      * If a backup does not meet the specified requirements the
      * UnHealthyBackupWasFound event will be fired.
      */
-    'monitor_backups' => [
-        [
-            'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['local'],
-            'health_checks' => [
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
-            ],
-        ],
+  'monitor_backups' => [
+    [
+      'name' => env('APP_NAME', 'laravel-backup'),
+      'disks' => ['local'],
+      'health_checks' => [
+        \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
+        \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+      ],
+    ],
 
-        /*
+    /*
         [
             'name' => 'name of the second app',
             'disks' => ['local', 's3'],
@@ -228,10 +228,10 @@ return [
             ],
         ],
         */
-    ],
+  ],
 
-    'cleanup' => [
-        /*
+  'cleanup' => [
+    /*
          * The strategy that will be used to cleanup old backups. The default strategy
          * will keep all backups for a certain amount of days. After that period only
          * a daily backup will be kept. After that period only weekly backups will
@@ -240,41 +240,41 @@ return [
          * No matter how you configure it the default strategy will never
          * delete the newest backup.
          */
-        'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
+    'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
 
-        'default_strategy' => [
+    'default_strategy' => [
 
-            /*
+      /*
              * The number of days for which backups must be kept.
              */
-            'keep_all_backups_for_days' => 7,
+      'keep_all_backups_for_days' => 7,
 
-            /*
+      /*
              * The number of days for which daily backups must be kept.
              */
-            'keep_daily_backups_for_days' => 16,
+      'keep_daily_backups_for_days' => 16,
 
-            /*
+      /*
              * The number of weeks for which one weekly backup must be kept.
              */
-            'keep_weekly_backups_for_weeks' => 8,
+      'keep_weekly_backups_for_weeks' => 8,
 
-            /*
+      /*
              * The number of months for which one monthly backup must be kept.
              */
-            'keep_monthly_backups_for_months' => 4,
+      'keep_monthly_backups_for_months' => 4,
 
-            /*
+      /*
              * The number of years for which one yearly backup must be kept.
              */
-            'keep_yearly_backups_for_years' => 2,
+      'keep_yearly_backups_for_years' => 2,
 
-            /*
+      /*
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              */
-            'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
-        ],
+      'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
     ],
+  ],
 
 ];
